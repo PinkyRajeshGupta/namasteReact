097d4617f7fcd85c67d56df27a17b40d82feaa3f
Pinky Gupta <pinky@quantique.ai>
2024-03-27 11:08:13 +0530

    secondvedio-pinky

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # NamestaReact
+
+# NamestaReact
+
+# Parcel
+
+-Dev Buil
+-Local Server
+-HMR = Hot Module REplaement
+-file watching algorithem-writtenin c++
+-caching-faster builds
+-image optimization
+-minification on files
+-bundling
+-compress
+-consistent Hashing
+code Splitting
+-Differentail Bundling
+-Diagnostic
+error handaling
+https
+tree shaking
+difference built for dev and produc
--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const header = React.createElement("h1", { id: "header" }, "hi this is h1");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//const header = React.createElement("h1", { id: "header" }, "hi this is h1");
+const header2 = React.createElement("div", {}, [
+  React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "hi this is first div child ")
+  ),
+  React.createElement("div", {}, "this is second div"),
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(header);
+root.render(header2);
